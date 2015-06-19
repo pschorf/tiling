bc12585660673e58a37bcdcb5ea25e1a825edf15
Paul Schorfheide <pschorf2@gmail.com>
2015-06-18 21:09:33 -0500

    try to parse url

--- src/tiling/db/core.clj
+++ src/tiling/db/core.clj
@@ -1,10 +1,22 @@
 (ns tiling.db.core
-  (:require [yesql.core :refer [defqueries]]))
+  (:require [[yesql.core :refer [defqueries]]
+             [clojure.string :refer [split]]])
+  (:import java.net.URI))
 
-(def db-spec {:subprotocol "postgresql"
-              :subname "//localhost/tiling"
-              :user "towner"
-              :password "tiling"})
+(def db-spec (let [uri (URI. (System/getenv "DATABASE_URL"))
+                   user (first (split (.getUserInfo uri) #":"))
+                   pass (second (split (.getUserInfo uri #":")))
+                   host (.getHost uri)
+                   path (.getPath uri)]
+               {:subprotocol "postgresql"
+                :subname (str "//" host path)
+                :user user
+                :password pass}))
+
+;; (def db-spec {:subprotocol "postgresql"
+;;               :subname "//localhost/tiling"
+;;               :user "towner"
+;;               :password "tiling"})
 
 (defqueries "sql/tiling.sql" {:connection db-spec})
 
